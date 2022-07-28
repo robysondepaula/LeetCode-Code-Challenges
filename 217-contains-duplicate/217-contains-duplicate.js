f339abefe09cfbd951496440748c105329024e0c
Robyson De Paula <66643020+robysondepaula@users.noreply.github.com>
2022-07-28 16:17:31 -0400

    Time: 242 ms (20.87%), Space: 51.3 MB (32.58%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -13,6 +13,8 @@ function containsDuplicate (nums) {
     
     nums.sort((a,b)=>{return a - b});
     for(let i = 0; i < nums.length; i ++){
+        //check here i > 0 because the size needs to be greater then 1 we cannot compare the i with something that does not exists
+        //Check if the nums are equals true for equal and false for not equal
         if(i > 0 && nums[i - 1] === nums[i]) return true;
     }
     return false;
@@ -26,4 +28,4 @@ function containsDuplicate (nums) {
 //Output: true
 //Input: nums = [1,2,3,4]
 //Output: false
-//Big O : time complexty O(n), memory complexity O(n)
+//Big O : time complexty O(n), memory complexity O(1)
