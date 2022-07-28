af2bfef72e94919350dbd9e904ea6a0128d80645
Robyson De Paula <66643020+robysondepaula@users.noreply.github.com>
2022-07-28 16:32:29 -0400

    Time: 243 ms (20.85%), Space: 51 MB (50.57%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -4,21 +4,20 @@
  */
 function containsDuplicate (nums) {
     
+    //If the size of the array is less than 1 return false; 
+    //Do a loop with an index i to go through each element of the array
+    //Sort the array ex: get the [1,2,3,1] sort to [ 1, 1, 2, 3 ]
+    //Now with [ 1, 1, 2, 3 ] if i === i + return true else false
+    //This case 1 === 1 so return true
     
-   // nums = [1,2,3,1]
-   // sort the array  to nums = [1,1,2,3] In this case the repeated element will be next to the first, and it will be ascending order, maybe we do not need to walk thrhouh all the arrayt=
-   //Go through array and see if what i see now, is something that i have seen before 
-   //For loop to walk thorugh the array
-    
-    
-    nums.sort((a,b)=>{return a - b});
-    for(let i = 0; i < nums.length; i ++){
-        //check here i > 0 because the size needs to be greater then 1 we cannot compare the i with something that does not exists
-        //Check if the nums are equals true for equal and false for not equal
-        if(i > 0 && nums[i - 1] === nums[i]) return true;
+    if (nums.length < 1) return false;
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return true;
     }
-    return false;
-
+  }
+  return false;
 };
 
 
