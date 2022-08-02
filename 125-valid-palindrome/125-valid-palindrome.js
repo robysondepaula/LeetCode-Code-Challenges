1973ececfcadba96d22b242e22584daf23b490e4
Robyson De Paula <66643020+robysondepaula@users.noreply.github.com>
2022-08-02 13:29:48 -0400

    Time: 121 ms (37.63%), Space: 44.6 MB (70.61%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = s => {
+  s = s.toLowerCase().replace(/[^a-z0-9]/gi,'');
+  for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+    if (s.charAt(i) !== s.charAt(j)) return false;
+  }
+  return true;
+}
+//BigO time complexity O(n) / memory complexity O(1)
