63420abceb12121d1ebf8f53c5b11deb9c2bb2d7
Robyson De Paula <66643020+robysondepaula@users.noreply.github.com>
2022-08-02 13:31:13 -0400

    Time: 126 ms (31.97%), Space: 44.6 MB (67.54%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -12,11 +12,16 @@ var isPalindrome = function(s) {
     //Check if s is string or number, use a regex to exclude special chars and replace no space, if it is not a string, then convert it to string
     //Run the array doing a loop with i as index run half of s if s[i] is different of s.length - i - 1 just return false, otherwise return true;
     
-  s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] !== s[s.length - 1 - i]) return false;
-    }
-    return true;
+    
+  
+    s=(typeof(s)==="string") ?  s.replace(/[^A-Za-z0-9]/g, '').toLowerCase() : s.toString();
+   
+    for(let i = 0; i < s.length/2; i ++){
+        if(s[i] !== s[s.length - 1 - i])  return false;
+        
+    }return true;
 };
+
+
 
 //BigO time complexity O(n) / memory complexity O(1)
