d563609bd316e9af2532d9fbbf2c100758602213
Robyson De Paula <66643020+robysondepaula@users.noreply.github.com>
2022-08-04 11:44:42 -0400

    Time: 93 ms (53.64%), Space: 43.1 MB (20.00%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -21,13 +21,18 @@ var isValid = function(s) {
     
     for(let char of s){
         if(map[char]){
+            //char is the opening bracket
             stack.push(map[char])
         }else if (stack.length > 0 && stack[stack.length - 1] === char){
+            //char here is the closing bracket and the first stack match
             stack.pop();
         }else{
+            //char is the closing bracket and the first stack doesnt match
             return false;
         }
     }
     return stack.length === 0;
    
 };
+
+//Big O: time complexity O(n), memory complexity O(n)
